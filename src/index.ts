d574d61f447b308658842531646d29c158da8682
take-ookubo <ookubo-t@appleworld.co.jp>
2022-06-01 11:02:51 +0900

    Move index.ts to src/index.ts for nodemon

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,20 @@ fastify.get('/', function (request, reply) {
   reply.send({ hello: 'world' })
 })
 
+interface IHelloParams {
+  id: number;
+  name: string;
+}
+
+fastify.get<{Params: IHelloParams}>('/hello/:id/:name', (request, reply) => {
+  reply.send({
+    req: {
+      id: request.params.id + 1,
+      name: request.params.name,
+    }
+  })
+})
+
 // Run the server!
 fastify.listen(3000, function (err, address) {
   if (err) {
