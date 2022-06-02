fcab24baf34d2c03833000eda08a10913b789230
take-ookubo <ookubo-t@appleworld.co.jp>
2022-06-02 20:35:07 +0900

    Top level await を避けるためアロー関数配下に追加

--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,36 @@ const fastify = Fastify({
   logger: true
 })
 
-// Declare a route
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
-})
 
 interface IHelloParams {
   id: number;
   name: string;
 }
 
-fastify.get<{Params: IHelloParams}>('/hello/:id/:name', (request, reply) => {
-  reply.send({
-    req: {
-      id: request.params.id + 1,
-      name: request.params.name,
-    }
+// Top level await を避けるためアロー関数配下に追加
+const main = async () => {
+  // Declare a route
+  fastify.get('/', function (request, reply) {
+    reply.send({ hello: 'world' })
   })
-})
 
-// Run the server!
-fastify.listen(3000, function (err, address) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-  // Server is now listening on ${address}
-})
+  fastify.get<{Params: IHelloParams}>('/hello/:id/:name', (request, reply) => {
+    reply.send({
+      req: {
+        id: request.params.id + 1,
+        name: request.params.name,
+      }
+    })
+  })
+
+  // Run the server!
+  fastify.listen(3000, function (err, address) {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+    // Server is now listening on ${address}
+  })
+}
+
+main()
