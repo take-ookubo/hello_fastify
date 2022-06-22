cee066378a53621c895c3b28ad653f1fe0be14bb
take-ookubo <ookubo-t@appleworld.co.jp>
2022-06-22 13:39:53 +0900

    npx prettier --write ./src/

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,36 @@
-import { PrismaClient } from "@prisma/client";
-import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client"
+import Fastify from "fastify"
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 const fastify = Fastify({
   logger: true,
-});
+})
 
 interface IHelloParams {
-  id: number;
-  name: string;
-  email: string;
+  id: number
+  name: string
+  email: string
 }
 
 // Top level await を避けるためアロー関数配下に追加
 const main = async () => {
   // Declare a route
   fastify.get("/", function (request, reply) {
-    reply.send({ hello: "world" });
-  });
+    reply.send({ hello: "world" })
+  })
 
-  fastify.get<{ Params: IHelloParams }>(
-    "/hello/:id",
-    async (request, reply) => {
-      const user = await prisma.user.findUnique({
-        where: { id: Number(request.params?.id) },
-      });
-      reply.send({
-        req: {
-          id: user?.id,
-          name: user?.name,
-          email: user?.email,
-        },
-      });
-    }
-  );
+  fastify.get<{ Params: IHelloParams }>("/hello/:id", async (request, reply) => {
+    const user = await prisma.user.findUnique({
+      where: { id: Number(request.params?.id) },
+    })
+    reply.send({
+      req: {
+        id: user?.id,
+        name: user?.name,
+        email: user?.email,
+      },
+    })
+  })
 
   // NOTE: curl http://localhost:3000/hello -X POST -H "Content-Type: application/json" -d '{"name":"test", "email": "test@example.com"}'
   fastify.post<{ Body: IHelloParams }>("/hello", async (request, reply) => {
@@ -43,31 +40,31 @@ const main = async () => {
           name: request.body.name,
           email: request.body.email,
         },
-      });
+      })
       reply.send({
         req: {
           id: newUser.id,
           name: newUser.name,
           email: newUser.email,
         },
-      });
+      })
     } catch (e) {
-      console.log(e);
+      console.log(e)
       if (e.code == "P2002") {
-        reply.code(400).send(`Not unique email: ${request.body.email}`);
+        reply.code(400).send(`Not unique email: ${request.body.email}`)
       } else {
-        reply.code(500).send(" Internal Server Error");
+        reply.code(500).send(" Internal Server Error")
       }
     }
-  });
+  })
   // Run the server!
   fastify.listen(3000, function (err) {
     if (err) {
-      fastify.log.error(err);
-      process.exit(1);
+      fastify.log.error(err)
+      process.exit(1)
     }
     // Server is now listening on ${address}
-  });
-};
+  })
+}
 
-main();
+main()
